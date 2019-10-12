52887b723ce4c8816b4c40efd6c1812a9ef0a4d7
Sabbir Hossain <cse.sabbir@gmail.com>
2019-10-12 06:11:10 +0600

    improve schema

--- demo4.js
+++ demo4.js
@@ -2,6 +2,19 @@ const { process } = require("./lib/generator");
 
 const schema = {
   value: [["text"]],
+  val2: [
+    {
+      a: "4...10",
+      b: "ad,adfa,asda",
+      c: "text"
+    }
+  ],
+  val3: ["asd", "43aaf", "dfawef"],
+  val4: {
+    __type: "array",
+    __range: "1,3",
+    __property: "a,b,c,d"
+  },
   category_id: {
     __type: "array",
     __range: "1,3",
@@ -13,7 +26,37 @@ const schema = {
       b: "float"
     }
   },
-  d: "100...120"
+  d: "100...120",
+  domain: "url",
+  status: "active,inActive",
+  info: {
+    aa: {
+      bb: {
+        cc: {
+          dd: {
+            ee: {
+              ff: {
+                gg: {
+                  hh: {
+                    ii: {
+                      jj: {
+                        kk: {
+                          ll: {
+                            __type: "text"
+                          },
+                          h: ["a", "b"]
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 };
 
 const data = process(schema);
