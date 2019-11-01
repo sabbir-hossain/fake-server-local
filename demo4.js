005420654839cbb594202898715d77aaf6776adc
Sabbir Hossain <cse.sabbir@gmail.com>
2019-11-02 04:56:19 +0600

    show toastr

--- demo4.js
+++ demo4.js
@@ -28,37 +28,43 @@ const schema = {
   },
   d: "100...120",
   domain: "url",
-  status: "active,inActive",
-  info: {
-    aa: {
-      bb: {
-        cc: {
-          dd: {
-            ee: {
-              ff: {
-                gg: {
-                  hh: {
-                    ii: {
-                      jj: {
-                        kk: {
-                          ll: {
-                            __type: "text"
-                          },
-                          h: ["a", "b"]
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+  status: "active,inActive"
 };
 
-const data = process(schema);
+// const data = process(schema);
 
-console.log(JSON.stringify(data, null, 2));
+// console.log(JSON.stringify(data, null, 2));
+
+const schema22 = {
+  value: [["text"]],
+  val2: [
+    {
+      a: "4...10",
+      b: "ad,adfa,asda",
+      c: "text"
+    }
+  ],
+  val3: ["asd", "43aaf", "dfawef"],
+  val4: {
+    __type: "array",
+    __range: "1,3",
+    __property: "a,b,c,d"
+  },
+  category_id: {
+    __type: "array",
+    __range: "1,3",
+    __property: {
+      a: {
+        __type: "title",
+        __range: 2
+      },
+      b: "float"
+    }
+  },
+  d: "100...120",
+  domain: "url",
+  status: "active,inActive"
+};
+
+// const data = new Set(Object.keys(schema22));
+// console.log({ data });
--- helpers/util.js
+++ helpers/util.js
@@ -52,11 +52,56 @@ const getMinMax = (__range = "") => {
   }
 };
 
+function checkObject(obj1, obj2) {
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+
+  if (obj1Keys.length !== obj2Keys.length) {
+    return false;
+  }
+
+  let isEqual = false;
+  for (let i = 0, len = obj1Keys.length; i < len; i++) {
+    const key1 = obj1Keys[i] || "";
+    const key2 = obj2Keys[obj2Keys.indexOf(key1) || 0] || "";
+    if (key2 === "" || key1 === "") {
+      return false;
+    } else if (
+      typeof obj2[key2] === "undefined" ||
+      typeof obj1[key1] === "undefined"
+    ) {
+      return false;
+    } else if (typeof obj1[key1] === "object") {
+      isEqual = checkObject(obj1[key1], obj2[key2]);
+      if (!isEqual) {
+        return false;
+      }
+    } else if (typeof obj1[key1] === "function") {
+      if (obj1[key1].toString() !== obj2[key2].toString()) {
+        return false;
+      } else {
+        isEqual = true;
+      }
+    } else if (obj1[key1] === obj2[key2]) {
+      isEqual = true;
+    } else {
+      return false;
+    }
+  }
+
+  if (isEqual) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 module.exports = {
   uuid,
   getMinMax,
   tokenEncode,
   tokenDecode,
+  checkObject,
   randomNumberGenerator,
   throwErrorForEmptyValue
 };
